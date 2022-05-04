9f5aadce9e14fbdf4bea622c1445e06c7a4feb46
AdrianDanko <102218518+AdrianDanko@users.noreply.github.com>
2022-05-04 15:27:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function vypocty(cislo1 = 4,cislo2 = 2) {
+    console.log('Vstupné parametre')
+    console.log(cislo1, cislo2)
+    console.log("Vykonávam matematické operácie")
+    console.log(cislo1 +" + "+ cislo2 +" = " + (cislo1 + cislo2))
+    console.log(cislo1 +" - "+ cislo2 +" = " + (cislo1 - cislo2))
+    console.log(cislo1 +" * "+ cislo2 +" = " + (cislo1 * cislo2))
+    console.log(cislo1 +" / "+ cislo2 +" = " + (cislo1 / cislo2))
+}
+console.log(vypocty());
